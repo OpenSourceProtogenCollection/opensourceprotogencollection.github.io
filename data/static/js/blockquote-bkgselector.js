ce8bffb28dca8aa7e5cfdc2ce730a0f41d1c4aa2
TitusStudiosMediaGroup <titusstudiosmediagroup@gmail.com>
2020-06-26 21:36:18 +1200

    dev

---
+++ data/static/js/blockquote-bkgselector.js
@@ -0,0 +1,4 @@
+$('blockquote:contains("NOTE: ")').css('background-color', 'var(--backquote-bkg)');
+$('blockquote:contains("INFO: ")').css('background-color', 'var(--backquote-bkg-indigo)');
+$('blockquote:contains("WARNING: ")').css('background-color', 'var(--backquote-bkg-red)');
+$('blockquote:contains("CAUTION: ")').css('background-color', 'var(--backquote-bkg-orange)');
